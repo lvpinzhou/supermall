cadb50ae35fd2e7e733a8e6d439034591805be41
Tony <1486114909@qq.com>
2021-07-29 16:37:05 +0800

    2021.7.29

---
+++ src/network/request.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+export function request(config) {
+    //1.创建axios的实例
+    const instance = axios.create({
+        baseURL: 'http://2021.5.20:8000',
+        timeout: 5000
+    })
+
+    //2.axios的拦截器
+    //2.1.请求拦截的作用
+    instance.interceptors.request.use(config => {
+        return config
+    },err => {
+        console.log(err);
+    })
+
+    //3.发送真正的网络请求
+    return instance(config)
+}
---
+++ src/views/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="home">
+        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    </div>
+</template>
+
+<script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from "network/home";
+export default {
+    name: "Home",
+    components: {
+        NavBar 
+    },
+    created() {
+        //1.请求多个数据
+        getHomeMultidata().then(res => {
+            console.log(res);
+        })
+    }
+}
+</script>
+
+<style scoped>
+  .home-nav {
+      background-color: var(--color-tint);
+      color: #fff;
+  }
+</style>
