6a33c996bd91bf031e9e56e46f42bd1410f72755
Tony <1486114909@qq.com>
2021-08-03 09:06:31 +0800

    新增商品列表、轮动图

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="wapp" ref="wrapper">
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+import BScroll from 'better-scroll'
+export default {
+    name: "Scroll",
+    data() {
+        return {
+            scroll: null
+        }
+    },
+    mounted() {
+        setTimeout(function(){
+            this.scroll = new BScroll(document.querySelector('.wapp'), {
+                observeImage: true,
+                ObserveDom:true,
+                click:true
+        })
+        },5000);
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,134 @@
 <template>
-    <div id="home">
-        <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    </div>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <scroll class="content">
+        <HomeSwiper :banners="banners" />
+        <recommend-view :recommends="recommends" />
+        <feature-view />
+        <tab-control class="tab-control" :titles="['流行','新款','精选']" @tabClick="tabClick" />
+        <good-list :goods="showGoods" />
+    </scroll>  
+  </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from "network/home";
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
+
+import NavBar from "components/common/navbar/NavBar";
+import Scroll from "components/common/scroll/Scroll";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodsList";
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
+
 export default {
-    name: "Home",
-    components: {
-        NavBar 
-    },
-    created() {
-        //1.请求多个数据
-        getHomeMultidata().then(res => {
-            console.log(res);
-        })
+  name: "Home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    TabControl,
+    GoodList,
+    Scroll
+  },
+  data() {
+    return {
+      // result: null
+      banners: [],
+      recommends: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      },
+      currentType: "pop"
+    };
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     }
-}
+  },
+  created() {
+    //1.请求多个数据
+    this.getHomeMultidata();
+
+    //2.请求商品数据
+    this.getHomeGoods("pop");
+
+    this.getHomeGoods("new");
+
+    this.getHomeGoods("sell");
+  },
+  methods: {
+    /**
+     * 事件监听相关方法
+     */
+    tabClick(index) {
+      console.log(index);
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
+      }
+    },
+    /**
+     * 网络请求相关方法
+     */
+
+    getHomeMultidata() {
+      getHomeMultidata().then(res => {
+        console.log(res);
+        // this.result = res;
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then(res => {
+        console.log(res);
+        this.goods[type].list.push(...res.data.data.list);
+        this.goods[type].page += 1;
+      });
+    }
+  }
+};
 </script>
 
 <style scoped>
-  .home-nav {
-      background-color: var(--color-tint);
-      color: #fff;
-  }
+#home {
+  padding-top: 44px;
+  height: 100vh;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9;
+}
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+.content {
+    height: calc(100% - 93px);
+    overflow: hidden;
+}
 </style>
